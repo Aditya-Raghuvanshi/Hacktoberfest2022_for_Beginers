45eec0328cf9638bc2ad02e4e59b18ea59f81087
Aditya-Raghuvanshi <103103243+Aditya-Raghuvanshi@users.noreply.github.com>
2022-10-25 12:37:47 +0530

    Add files via upload
    this code will display 2D string

---
+++ Question 3.c
@@ -0,0 +1,25 @@
+//Write a program to read and display a 2D array of strings in C language.
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+    char a[4][20];
+    int i,j;
+    printf("enter 4 strings ");
+    for(i=0;i<4;i++)
+    {
+        fgets(a[i],20,stdin);
+    }
+    printf("you entered : \n");
+    for(i=0;i<4;i++)
+    {
+        int l=strlen(a[i]);
+        for(j=0;j<=l-2;j++)
+        {
+            printf("%c",a[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
